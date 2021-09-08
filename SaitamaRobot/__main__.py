51338f26eb9af7ebad7867dc6cd530b45d8069be
abai254 <84307584+abai254@users.noreply.github.com>
2021-09-08 16:48:09 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -303,7 +303,7 @@ def help_button(update, context):
                 parse_mode=ParseMode.MARKDOWN,
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="Back", callback_data="help_back")]]
+                    [[InlineKeyboardButton(text="◀️", callback_data="help_prev")], [InlineKeyboardButton(text="Back", callback_data="help_back")], [InlineKeyboardButton(text="▶️", callback_data="help_next")]]
                 ),
             )
 
