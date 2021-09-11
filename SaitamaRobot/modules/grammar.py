253837fb3e5009a3bcc271fd47f4050d72a6e605
abai254 <84307584+abai254@users.noreply.github.com>
2021-09-11 17:07:18 +0530

    Update grammar.py

--- SaitamaRobot/modules/grammar.py
+++ SaitamaRobot/modules/grammar.py
@@ -1,44 +1,37 @@
-
 import json
 from pprint import pprint
 
 import requests
-from telegram import Update, Bot
 from telegram.ext import CommandHandler
 
-from SaitamaRobot import dispatcher
+from AnkiVector import dispatcher
 
 # Open API key
 API_KEY = "6ae0c3a0-afdc-4532-a810-82ded0054236"
 URL = "http://services.gingersoftware.com/Ginger/correct/json/GingerTheText"
 
 
-def translate(bot: Bot, update: Update):
+def grammar(update, context):
     if update.effective_message.reply_to_message:
         msg = update.effective_message.reply_to_message
 
-        params = dict(
-            lang="US",
-            clientVersion="2.0",
-            apiKey=API_KEY,
-            text=msg.text
-        )
+        params = dict(lang="US", clientVersion="2.0", apiKey=API_KEY, text=msg.text)
 
         res = requests.get(URL, params=params)
         # print(res)
         # print(res.text)
         pprint(json.loads(res.text))
-        changes = json.loads(res.text).get('LightGingerTheTextResult')
+        changes = json.loads(res.text).get("LightGingerTheTextResult")
         curr_string = ""
 
         prev_end = 0
 
         for change in changes:
-            start = change.get('From')
-            end = change.get('To') + 1
-            suggestions = change.get('Suggestions')
+            start = change.get("From")
+            end = change.get("To") + 1
+            suggestions = change.get("Suggestions")
             if suggestions:
-                sugg_str = suggestions[0].get('Text')  # should look at this list more
+                sugg_str = suggestions[0].get("Text")  # should look at this list more
                 curr_string += msg.text[prev_end:start] + sugg_str
 
                 prev_end = end
@@ -47,14 +40,15 @@ def translate(bot: Bot, update: Update):
         print(curr_string)
         update.effective_message.reply_text(curr_string)
 
-
 __help__ = """
- - /t: while replying to a message, will reply with a grammar corrected version
+Hi
 """
 
-__mod_name__ = "GRAMMAR"
+
+GRAMMAR_HANDLER = CommandHandler("gmr", grammar)
+
+dispatcher.add_handler(GRAMMAR_HANDLER)
 
 
-TRANSLATE_HANDLER = CommandHandler('t', translate)
-
-dispatcher.add_handler(TRANSLATE_HANDLER)
+__command_list__ = ["grammar"]
+__handlers__ = [GRAMMAR_HANDLER]
