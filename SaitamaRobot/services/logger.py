1b654503e120ce11a7a3c5ebd17248411cbb07b4
abai254 <84307584+abai254@users.noreply.github.com>
2021-09-12 15:50:40 +0530

    Update logger.py

--- SaitamaRobot/services/logger.py
+++ SaitamaRobot/services/logger.py
@@ -25,11 +25,11 @@ class InterceptHandler(logging.Handler):
 logging.basicConfig(handlers=[InterceptHandler()], level=logging.INFO)
 log = logging.getLogger(__name__)
 logger.add(
-    "logs/SaitamaRobot.log",
+    "logs/daisy.log",
     rotation="1 d",
     compression="tar.xz",
     backtrace=True,
     diagnose=True,
     level="INFO",
 )
-log.info("Enabled logging intro SaitamaRobot.log file.")
+log.info("Enabled logging intro daisy.log file.")
