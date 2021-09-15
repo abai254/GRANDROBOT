7714473a2647ed73a07ee16a1bcafdcc4e53adfe
abai254 <84307584+abai254@users.noreply.github.com>
2021-09-15 17:21:01 +0530

    Update handlers.py

--- tg_bot/modules/helper_funcs/handlers.py
+++ tg_bot/modules/helper_funcs/handlers.py
@@ -1,13 +1,14 @@
 from telegram import Update
 from telegram.ext import CommandHandler, RegexHandler, MessageHandler
+from pyrogram.types import Message
 
 import tg_bot.modules.sql.blacklistusers_sql as sql
 from tg_bot import ALLOW_EXCL
 
 if ALLOW_EXCL:
-    CMD_STARTERS = ('/', '!', '~', '.', '?')
+    CMD_STARTERS = ('/', '!', '~', '.', '?',)
 else:
-    CMD_STARTERS = ('/', '!', '~', '.', '?')
+    CMD_STARTERS = ('/',)
 
 
 class CustomCommandHandler(CommandHandler):
